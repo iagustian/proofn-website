26d3de25e0d744c1222fc94928403f8a374195fe
Ian Agustiawan <agustiawanian@outlook.com>
2015-12-02 14:53:31 +0700

    replaced landing page with new design from Mira, including request invitation form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,36 +1,26 @@
+/* ============================================================
+ * Plugin Core Init
+ * For DEMO purposes only. Extract what you need.
+ * ============================================================ */
 
 'use strict';
-$(document).ready(function(){
 
-	var toDate = $('[data-pages-countdown-date]').attr('data-pages-countdown-date');
-	$(".year").countdown(toDate, function(event) {
-		$(this).text(event.strftime('%Y'))
-	});
-	$(".month").countdown(toDate, function(event) {
-		$(this).text(event.strftime('%m'))
-	});
-	$(".day").countdown(toDate, function(event) {
-		$(this).text(event.strftime('%D'))
-	});
-	$(".hour").countdown(toDate, function(event) {
-		$(this).text(event.strftime('%H'))
-	});
-	$(".minutes").countdown(toDate, function(event) {
-		$(this).text(event.strftime('%M'))
-	});
-	// $(".seconds").countdown(toDate, function(event) {
-	// 	$(this).text(event.strftime('%S'))
-	// });
+//Intialize Slider
+var slider = new Swiper('#hero', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    parallax: true,
+    speed:1000
+});
 
-	//Intialize Slider
-    var slider = new Swiper('#inner-content-slider', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-				autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-        parallax: true,
-        speed: 1000
-    });
+//Intialize Testamonials
+var testimonials_slider = new Swiper('#testimonials_slider', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    parallax: true,
+    speed:1000
 });
